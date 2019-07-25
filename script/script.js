d576b929b2c2213dc071ec32bb2a1d1b404134ad
Abby Bahlmann <abbyb777@gmail.com>
2019-07-25 18:51:08 -0400

    needs more style and to be set up so gifs respond on-click

--- script/script.js
+++ script/script.js
@@ -1,23 +1,31 @@
-var queryURL= "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=3tDe0S1PsQj5U8TPiAv56eEWd2umYfKI&limit=5";
 var stillURL;
 var animateURL;
 var i=0;
+var userSearch;
 
-function createGIF(response){
-    for (i=0;i<response.data.length;i++){
-    var gifImage=$("<img>");
-    gifImage.attr("src",response.data[i].images.original_still.url);
-    gifImage.appendTo("#gif-results");
+function createGIF(x){
+    for (i=0;i<x.data.length;i++){
+    var gifImage=document.createElement("img");
+    $(gifImage).attr("width","300px");
+    $(gifImage).attr("data-still",x.data[i].images.original_still.url);
+    $(gifImage).attr("data-animate",x.data[i].images.original.url);
+    $(gifImage).attr("src",x.data[i].images.original.url);
+    $(gifImage).appendTo("#gif-results");
+    console.log("data-still",x.data[i].images.original_still.url)
     }
 }
 
+
+
 $("button").on("click", function(){
     $("#gif-start-message").hide();
-    var userInput=$("input").val();
-    console.log(userInput);
-    // $.ajax({
-    //     url:queryURL,
-    //     method:"GET"
-    // }).then(createGIF(response));
+    var userSearch=$("input").val();
+    var queryURL= "http://api.giphy.com/v1/gifs/search?q="+userSearch+"&api_key=3tDe0S1PsQj5U8TPiAv56eEWd2umYfKI&limit=5";
+
+    console.log(userSearch);
+    $.ajax({
+        url:queryURL,
+        method:"GET"
+    }).then(function(response){createGIF(response)});
 });
 
